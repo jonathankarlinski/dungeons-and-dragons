f1a3bdf6e489cfc0a7e9920825f9e8d33d7c6842
Jonathan <jonathankarlinski57@gmail.com>
2023-09-19 19:50:18 -0300

    refactor: Revisão de segurança apontada pelo sonarcloud

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,17 @@
+import * as crypto from 'crypto';
+
 function getRandomInt(min: number, max: number) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  const val = Math.random();
-  return Math.floor(val * (newMax - newMin)) + newMin;
+  const range = max - min;
+  if (range <= 0) {
+    throw new Error('max deve ser maior que min');
+  }
+
+  const randomBytes = crypto.randomBytes(4);
+  const randomInt = Math.floor(
+    (randomBytes.readUInt32BE(0) / 0xffffffff) * range + min,
+  );
+
+  return randomInt;
 }
 
 export default getRandomInt;
